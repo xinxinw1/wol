6a7b98ca34bf3aee017a7a8776afc5ac6ac7688c
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2015-10-10 14:11:26 -0400

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php header("Cache-Control: no-cache"); ?>
+<?php $ver = "1.0.0"; ?>
+<?php
+function issetgetall(){
+  for ($i = 0; $i < func_num_args(); $i++){
+    if (!isset($_GET[func_get_arg($i)])){
+      return false;
+    }
+  }
+  return true;
+}
+
+if (issetgetall('mac', 'ip', 'port')){
+  $mac = implode(':', str_split(preg_replace("/[^0-9a-zA-Z]/","", $_GET['mac']), 2));
+  $ip = $_GET['ip'];
+  $port = $_GET['port'];
+  echo shell_exec("./wol '$mac' -i '$ip' -p '$port'");
+}
+
+// localhost?mac=00:1A:92:AD:5C:19&ip=24.150.233.29&port=9
+?>
